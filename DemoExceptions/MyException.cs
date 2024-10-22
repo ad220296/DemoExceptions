0402d14d9cf3e3674e0538f9784acf052203c1f0
ad220296 <145586843+ad220296@users.noreply.github.com>
2024-10-22 19:29:26 +0200

    Add project files.

---
+++ DemoExceptions/MyException.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoExceptions
+{
+    public class MyException : ApplicationException
+    {
+        private int _errorCode;
+
+        public int Errorcode 
+        {
+            get
+            {
+                return _errorCode;
+            } 
+            set
+            {
+                _errorCode = value;
+            }
+        }
+        public MyException(int errorCode)
+        {
+            _errorCode = errorCode;
+        }
+        public MyException(int errorCode, string message)
+            :base(message)
+        {
+            _errorCode = errorCode;
+        }
+    }
+}
---
+++ DemoExceptions/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace DemoExceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // return;             -- wenn try nicht ausgefuehrt wird, auch kein finally
+            try
+            {
+                // return;         // -- wenn try ausgefuehrt wird, dann auch ein finally
+                Method_A();
+            }
+            catch (MyException ex)  //  keine Behandlung, zum naechsten catch
+            {
+
+            }
+            catch (Exception ex)    // Exception wird behandelt, daher wuerden auch keine weiteren catch's mehr behandelt
+            {
+                Console.WriteLine(ex.Message);
+                //throw;
+            }
+            finally
+            {
+                Console.WriteLine("Hello World");
+            }
+            Console.WriteLine("Nach dem try und catch");
+        }
+
+        static void Method_A()
+        {
+            ThrowAnException("Aufruf einer Methode ohne Handling");
+        }
+
+        static void ThrowAnException(string text)
+        {
+            throw new MyException(10, text);
+        }
+    }
+}
